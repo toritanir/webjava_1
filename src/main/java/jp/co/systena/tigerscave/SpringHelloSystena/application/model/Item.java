4c9d40ed205afe88697a194ed184499702f9a84c
toritanir <46831233+toritanir@users.noreply.github.com>
2019-01-23 06:16:06 +0900

    Update Item.java

--- src/main/java/jp/co/systena/tigerscave/SpringHelloSystena/application/model/Item.java
+++ src/main/java/jp/co/systena/tigerscave/SpringHelloSystena/application/model/Item.java
@@ -2,40 +2,25 @@ package jp.co.systena.tigerscave.SpringHelloSystena.application.model;
 
 public class Item {
 
-  // 名前
-  private String name;
-  private int price;
+ // 名前
+ private String name;
+ private int price;
 
-  public String getName() {
-    return this.name;
-  }
-  // setter
-  public void setName(String name) {
-    this.name = name;
-  }
-  public int getPrice() {
-    return price;
-  }
-  public void setprice(int price) {
-    this.price = price;
-  }
-  
+ public Item(){
+};
+ //getter.setter(name)
+ public String getName() {
+   return this.name;
+ }
+ public void setName(String name) {
+   this.name = name;
+ }
+ //getter.setter(price)
+ public int getPrice() {
+   return price;
+ }
+ public void setPrice(int price) {
+   this.price = price;
+ }
 
-
-public void createProtein() {
-  this.name = "プロテイン";
-  this.price = 3000;
-}
-public void createAbRoller() {
-  this.name = "腹筋ローラー";
-  this.price = 1500;
-}
-public void createDumbbell() {
-  this.name = "ダンベル";
-  this.price = 2000;
-}
-public void createMat() {
-  this.name = "ヨガマット";
-  this.price = 2500;
-}
 }
